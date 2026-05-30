2809ebdab0af77a74f66744336b3b60e2f29d6c7
abdisatar <kawhicoding@gmail.com>
2026-05-30 17:05:55 -0500

    init

---
+++ frontend/breadBank/src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <!-- Your content here -->
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+</script>
